d05f8f4e54fe1423ea92d5aa2d3a4aa454ed0b52
yandongquan <yandongquanlove@gmail.com>
2018-07-23 16:10:04 +0800

    :penguin: add activemq-demo

---
+++ activemq-demo/src/main/java/com/javakz/consumer/QueueConsumer.java
@@ -0,0 +1,62 @@
+package com.javakz.consumer;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+import java.io.IOException;
+
+/**
+ *  .--,       .--,
+ * ( (  \.---./  ) )
+ *  '.__/o   o\__.'
+ *     {=  ^  =}
+ *      >  -  <
+ *     /       \
+ *    //       \\
+ *   //|   .   |\\
+ *   "'\       /'"_.-~^`'-.
+ *      \  _  /--'         `
+ *    ___)( )(___
+ *   (((__) (__)))    高山仰止,景行行止.虽不能至,心向往之。
+ * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ *
+ * @Author: yandq
+ * @Description: 点对点-消费者
+ * @Date: Create in 9:59 2018/7/23
+ * @Modified By:
+ */
+public class QueueConsumer {
+    public static void main(String[] args) throws JMSException, IOException {
+        // 创建连接工厂
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory("tcp://192.168.2.129:61616");
+        // 获取连接
+        Connection connection = activeMQConnectionFactory.createConnection();
+        // 启动连接
+        connection.start();
+        // 获取 session
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        // 创建队列对象
+        Queue queue = session.createQueue("first-queue");
+        // 创建消息消费者
+        MessageConsumer consumer = session.createConsumer(queue);
+        consumer.setMessageListener(new MessageListener() {
+            @Override
+            public void onMessage(Message message) {
+                // 获取文本消息对象
+                TextMessage textMessage = (TextMessage)message;
+                try {
+                    String text  = textMessage.getText();
+                    System.out.println("消费者收到消息，内容："+text);
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        // 等待键盘输入
+        System.in.read();
+        // 关闭资源
+        consumer.close();
+        session.close();
+        connection.close();
+    }
+}
---
+++ activemq-demo/src/main/java/com/javakz/producer/QueueProducer.java
@@ -0,0 +1,53 @@
+package com.javakz.producer;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+/**
+ *  .--,       .--,
+ * ( (  \.---./  ) )
+ *  '.__/o   o\__.'
+ *     {=  ^  =}
+ *      >  -  <
+ *     /       \
+ *    //       \\
+ *   //|   .   |\\
+ *   "'\       /'"_.-~^`'-.
+ *      \  _  /--'         `
+ *    ___)( )(___
+ *   (((__) (__)))    高山仰止,景行行止.虽不能至,心向往之。
+ * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ *
+ * @Author: yandq
+ * @Description: 点对点-消息生产者
+ * @Date: Create in 9:56 2018/7/23
+ * @Modified By:
+ */
+public class QueueProducer {
+
+    public static void main(String[] args) throws JMSException {
+        // 创建连接工厂
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory("tcp://192.168.2.129:61616");
+        // 获取连接
+        Connection connection = activeMQConnectionFactory.createConnection();
+        // 启动连接
+        connection.start();
+        // 获取 session
+        // 第一个参数：是否开启事务。true：开启事务，第二个参数忽略。
+        // 第二个参数：当第一个参数为false时，才有意义。消息的应答模式。1、自动确认；2、客户端手动确认；3、自动批量确认；0、事务提交并确认。一般是自动确认。
+        Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+        // 创建队列对象
+        Queue queue = session.createQueue("first-queue");
+        // 创建消息生产者对象
+        MessageProducer producer = session.createProducer(queue);
+        // 创建消息
+        TextMessage textMessage = session.createTextMessage("这是我的第一个消息，请查收。");
+        // 使用生产者发送消息
+        producer.send(textMessage);
+        // 关闭资源
+        producer.close();
+        session.close();
+        connection.close();
+    }
+}
